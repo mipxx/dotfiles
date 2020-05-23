08d3800114ca7b64de0296f692188dcf3ac6d6f4
Michael Paul <michael.paul@mailbox.org>
2020-05-23 21:21:57 +0200

    update zsh

--- .config/zsh/01-environment.zsh
+++ .config/zsh/01-environment.zsh
@@ -18,7 +18,9 @@ export LC_TIME=en_DK.UTF-8
 
 export XDG_CONFIG_HOME=~/.config
 export XDG_DATA_HOME=~/.local/share
+export XDG_CACHE_HOME=~/.cache
 
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export KDEHOME="$XDG_CONFIG_HOME"/kde
+export WGETRC="$XDG_CONFIG_HOME"/wgetrc
