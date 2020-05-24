0f1dcf3058d6b0f3eceb07ad3cd314d7df33a1b7
Michael Paul <mipxx@users.noreply.github.com>
2020-05-24 13:27:03 +0200

    Update 01-environment.zsh

--- .config/zsh/01-environment.zsh
+++ .config/zsh/01-environment.zsh
@@ -1,20 +1,9 @@
 export EDITOR=vim
 export VISUAL=vim
 
-export LANG=en_DK.UTF-8
-export LANGUAGE=en_DK.UTF-8
-export LC_ADDRESS=de_DE.UTF-8
-export LC_COLLATE=C
-export LC_CTYPE=en_DK.UTF-8
-export LC_IDENTIFICATION=
-export LC_MEASUREMENT=en_DK.UTF-8
-export LC_MESSAGES=C
-export LC_MONETARY=de_DE.UTF-8
-export LC_NAME=en_DK.UTF-8
-export LC_NUMERIC=en_DK.UTF-8
-export LC_PAPER=en_DK.UTF-8
-export LC_TELEPHONE=de_DE.UTF-8
-export LC_TIME=en_DK.UTF-8
+export LANG=de_DE.UTF-8
+export LANGUAGE=de_DE.UTF-8
+export LC_MESSAGES=en_US.UTF-8
 
 export XDG_CONFIG_HOME=~/.config
 export XDG_DATA_HOME=~/.local/share
