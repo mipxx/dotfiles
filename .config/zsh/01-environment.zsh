7fe2c4085a775e53dc8cb158ba1990960c91742f
Michael Paul <mipxx@users.noreply.github.com>
2020-05-30 19:52:52 +0200

    add pyenv

--- .config/zsh/01-environment.zsh
+++ .config/zsh/01-environment.zsh
@@ -11,3 +11,11 @@ export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export KDEHOME="$XDG_CONFIG_HOME"/kde
 export WGETRC="$XDG_CONFIG_HOME"/wgetrc
+
+# https://github.com/pyenv/pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
